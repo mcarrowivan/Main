b85544f3d4dde0f53b9a7ab435666f496bdb2c19
mcarrowivan <mcarrowivan@mail.ru>
2022-09-28 21:00:49 +0300

    f2

--- Machine_learning_2022/submission.py
+++ Machine_learning_2022/submission.py
@@ -23,3 +23,30 @@ class f1:
         """
         h = 2
         return h
+
+
+class f2:
+    def __call__(self, x: float):
+        a = numpy.sin(3 * numpy.sqrt(x ** 3) + 2) + x ** 2
+        return a
+
+    def grad(self, x: float):
+        """
+        Args :
+            x : float
+        Returns :
+            float
+        """
+        d = 0.5 * x * (9 * x / numpy.sqrt(x ** 3) + 4) * numpy.cos(3 * numpy.sqrt(x ** 3) + x ** 2 + 6)
+        return d
+
+    # def hess(self, x: float):
+    #     """
+    #     Args :
+    #         x : float
+    #     Returns :
+    #         float
+    #     """
+    #     ...
+    #
+    #     return
