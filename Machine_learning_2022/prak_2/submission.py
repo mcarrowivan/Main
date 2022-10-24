94ee84a4bc7aa0ea53feb03ced181ddf3e219db9
mcarrowivan <mcarrowivan@mail.ru>
2022-10-24 21:21:59 +0300

    q

--- Machine_learning_2022/prak_2/submission.py
+++ Machine_learning_2022/prak_2/submission.py
@@ -169,7 +169,6 @@ def find_best_split(feature_vector: np.ndarray, target_vector: np.ndarray, crite
                     max_col, max_val, max_gain = column, value, gain
 
         return split_values, np.array(gain_list), max_val, max_gain
-
     else:
         subset = np.random.choice(feature_vector, 10000, replace=True)
         max_col, max_val, max_gain = None, None, None
